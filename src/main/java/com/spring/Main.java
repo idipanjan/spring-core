c9159ec29b6078cbbae12e26632c77208419cf93
Dipanjan <dipanjanmaji45@gmail.com>
2024-06-08 00:03:54 +0530

    first commit

---
+++ src/main/java/com/spring/Main.java
@@ -0,0 +1,15 @@
+package com.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+  public static void main(String[] args) {
+
+    ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+    Student student1 = (Student) context.getBean("student1");
+    Student student2 = (Student) context.getBean("student2");
+    System.out.println(student1);
+    System.out.println(student2);
+  }
+}
---
+++ src/main/java/com/spring/Student.java
@@ -0,0 +1,28 @@
+package com.spring;
+
+public class Student {
+  private String sId;
+  private String sName;
+  private String sEmail;
+
+  public void setsId(String sId) {
+    this.sId = sId;
+  }
+
+  public void setsName(String sName) {
+    this.sName = sName;
+  }
+
+  public void setsEmail(String sEmail) {
+    this.sEmail = sEmail;
+  }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "sId='" + sId + '\'' +
+                ", sName='" + sName + '\'' +
+                ", sEmail='" + sEmail + '\'' +
+                '}';
+    }
+}
